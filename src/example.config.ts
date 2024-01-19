66d7a7662718543c645e9ec6e8b0f2173849299a
Kittipong-Dev <achikrab@gmail.com>
2024-01-20 00:38:44 +0700

    add database connection

---
+++ src/example.config.ts
@@ -0,0 +1,14 @@
+// rename to config.ts before build
+
+export default {
+    app: {
+        host: "",
+        port: ""
+    },
+    database: {
+        username: "",
+        password: "",
+        name: "",
+        host: ""
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
+import config from './config';
+import { dbConnect } from './db/database';
 
-const app = express();
-const port = 8000;
+async function main(): Promise<void> {
+    const app = express();
+    const port = config.app.port;
+    const host = config.app.host;
 
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+    await dbConnect();
+
+    app.use(json())
+    app.use(urlencoded({ extended: true}))
+
+    app.listen(port, () => console.log(`Server is running on http://${host}:${port}`))
+}
+main()
