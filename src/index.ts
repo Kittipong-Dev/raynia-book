952be5d9133573148a5ece11bf94b3eb1d3412dc
kittipong-dev <achikrab@gmail.com>
2024-02-12 23:09:29 +0700

    add routing

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express, { json, urlencoded } from 'express';
 import config from './config';
 import booksApi from './routers/booksApi';
 import accountsApi from './routers/accountsApi';
-// import routing from './routers/routing';
+import routing from './routers/routing';
 import { initDB } from './db/initDB';
+import path from 'path';
 
 async function main(): Promise<void> {
     const app = express();
@@ -14,14 +15,14 @@ async function main(): Promise<void> {
 
     app.use(json())
     app.use(urlencoded({ extended: true}))
-    app.use(express.static('../public'))
+    app.use(express.static(path.join(process.cwd(), 'public')))
 
     // api/v1
     app.use('/api/v1/books', booksApi)
     app.use('/api/v1/accounts', accountsApi)
 
     // /
-    // app.use('/', routing);
+    app.use('/', routing);
 
     app.listen(port, () => console.log(`Server is running on http://${host}:${port}`))
 }
--- src/routers/routing.ts
+++ src/routers/routing.ts
@@ -3,26 +3,29 @@ import path from 'path';
 
 const router = express.Router()
 
-router.get('/', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/index.html"))
-}) // Welcome
-router.get('/books', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/books.html"))
+router.get('/', (req: Request, res: Response) => { path.join(process.cwd(), './public/test/index.html')
+    res.sendFile(path.join(process.cwd(), './public/raynia.html'))
 })
-router.get('/books/book', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/book.html"))
+router.get('/welcome', (req: Request, res: Response) => {
+    res.sendFile(path.join(process.cwd(), './public/welcome.html'))
+})
+router.get('/books', (req: Request, res: Response) => {
+    res.sendFile(path.join(process.cwd(), './public/homepage.html'))
+})
+router.get('/books/book/:id', (req: Request, res: Response) => {
+    res.sendFile(path.join(process.cwd(), './public/book.html'))
 })
 router.get('/search', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/search.html"))
+    res.sendFile(path.join(process.cwd(), './public/search.html'))
 })
 router.get('/account', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/account.html"))
+    res.sendFile(path.join(process.cwd(), './public/account.html'))
 })
-router.get('/singIn', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/signIn.html"))
+router.get('/signIn', (req: Request, res: Response) => {
+    res.sendFile(path.join(process.cwd(), './public/signin.html'))
 })
-router.get('/signUp', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, "../../public/signUp.html"))
+router.get('/signup', (req: Request, res: Response) => {
+    res.sendFile(path.join(process.cwd(), './public/signup.html'))
 })
 
 export default router
