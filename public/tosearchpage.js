1a183f53c98e633770193ad7e1281ddb59f85474
ltaht <specter15564@gmail.com>
2024-02-12 21:02:25 +0700

    add path to search in every page

--- public/tosearchpage.js
+++ public/tosearchpage.js
@@ -9,9 +9,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
     document.querySelector('body').style.opacity = 1
 })
 
-var searchbar = document.getElementById("searchbar");
-    		if(searchbar) {
-      			searchbar.addEventListener("click", function (event) {
+var tosearch = document.getElementById("tosearch");
+    		if(tosearch) {
+      			tosearch.addEventListener("click", function (event) {
                     event.preventDefault(); // Prevent the default link behavior
     window.transitionToPage('search.html');
       			});
